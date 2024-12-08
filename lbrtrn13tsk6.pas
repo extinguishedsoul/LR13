a735333fa73df00913aa0a1efec88750fc0846d5
extinguishedsoul <vilordimarianna343@gmail.com>
2024-12-09 01:15:33 +0300

    Add files via upload

---
+++ lbrtrn13tsk6.pas
@@ -0,0 +1,53 @@
+﻿program lbrtrn13tsk6;
+var inputFile, tempFile: TextFile; chsl: array of Real; minIndex, maxIndex, i, schtchk: Integer; minValue, maxValue: Real; inputFilename, tempFilename: string;
+begin
+  inputFilename := 'C:\Users\ileof\OneDrive\Рабочий стол\lbrtrn13\input.txt';
+  tempFilename := 'C:\Users\ileof\OneDrive\Рабочий стол\lbrtrn13\temp.txt';
+  AssignFile(inputFile, inputFilename);
+  Reset(inputFile);
+  schtchk := 0;
+  while not Eof(inputFile) do
+  begin
+    SetLength(chsl, schtchk + 1);
+    ReadLn(inputFile, chsl[schtchk]);
+    Inc(schtchk);
+  end;
+  CloseFile(inputFile);
+  if schtchk = 0 then
+  begin
+    WriteLn('Файл пуст.');
+    Exit;
+  end;
+  minValue := chsl[0];
+  maxValue := chsl[0];
+  minIndex := 0;
+  maxIndex := 0;
+  for i := 1 to schtchk - 1 do
+  begin
+    if chsl[i] < minValue then
+    begin
+      minValue := chsl[i];
+      minIndex := i;
+    end;
+    if numbers[i] > maxValue then
+    begin
+      maxValue := chsl[i];
+      maxIndex := i;
+    end;
+  end;
+  AssignFile(tempFile, tempFilename);
+  Rewrite(tempFile);
+  for i := 0 to schtchk - 1 do
+  begin
+    if i = minIndex then
+      WriteLn(tempFile, maxValue) 
+    else if i = maxIndex then
+      WriteLn(tempFile, minValue)  
+    else
+      WriteLn(tempFile, numbers[i]); 
+  end;
+  CloseFile(tempFile);
+  Erase(inputFile);
+  Rename(tempFile, inputFilename);
+  WriteLn('Минимальный и максимальный элементы были успешно заменены.');
+end.
---
+++ lbrtrn13tsk7.pas
@@ -0,0 +1,20 @@
+﻿program lbrtrn13tsk7;
+var f: file of char; ch: char; i: integer;
+begin
+  Assign(f, 'C:\Users\ileof\OneDrive\Рабочий стол\lbrtrn13\text.txt'); 
+  Reset(f); 
+  i := 1;  
+  while not EOF(f) do
+  begin
+    Seek(f, i - 1); 
+    Read(f, ch);    
+    if i mod 2 = 0 then
+    begin
+      Seek(f, i - 1); 
+      ch := '!'; 
+      Write(f, ch); 
+    end;
+    Inc(i); 
+  end;  
+  Close(f); 
+end.
